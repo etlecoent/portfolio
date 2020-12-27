ee13f8238009248c66058ecd0da81140b0e34387
Etienne LC <etlecoent@gmail.com>
2020-12-27 10:04:01 -0500

    Started working on the navBar

---
+++ frontend/src/containers/navBar/index.js
@@ -0,0 +1,13 @@
+
+const NavBar = () => {
+  return (
+    <div id="navBar">
+      <span>logo</span>
+      <span>Home</span>
+      <span>Projects</span>
+      <span>Resume</span>
+    </div>
+  )
+};
+
+export default NavBar;
