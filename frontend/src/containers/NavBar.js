6908954d81982c2989ce04fb6bc424c36821daec
Etienne LC <etlecoent@gmail.com>
2020-12-30 04:38:00 -0500

    Added links

--- frontend/src/containers/NavBar.js
+++ frontend/src/containers/NavBar.js
@@ -9,13 +9,13 @@ const NavBar = (props) => {
 
   return (
     
-    <nav className="bg-white dark:bg-gray-800  shadow ">
+    <nav className="bg-white shadow dark:bg-gray-800">
       <div className="max-w-7xl mx-auto px-8">
         <div className="flex items-center justify-between h-16">
           <div className="w-full justify-between flex items-center">
-            <a className="flex-shrink-0" href="/">
+            <div className="flex-shrink-0">
               <img className="h-8 w-8" src={logo} alt="logo"/>
-            </a>
+            </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link to="/home">
