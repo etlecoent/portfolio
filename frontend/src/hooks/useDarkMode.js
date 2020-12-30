3a472e0784b64a678c4ba66c8bb33fe36101419d
Etienne LC <etlecoent@gmail.com>
2020-12-30 08:35:36 -0500

    Added useLocalStorage and useDarkMode hooks

---
+++ frontend/src/hooks/useDarkMode.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+const useDarkMode = () => {
+  
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
+
+  // if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  //   document.querySelector('html').classList.add('dark')
+  // } else {
+  //   document.querySelector('html').classList.remove('dark')
+  // }
+
+  const enabled =
+    typeof darkMode !== 'undefined' ? darkMode : false;
+
+  // Fire off effect that add/removes dark mode class
+  useEffect(() => {
+    const element = window.document.body;
+    if (enabled) {
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("light");
+    }
+  }, [enabled] // Only re-call effect when value changes
+  );
+
+  // Return enabled state and setter
+  return [darkMode, setDarkMode];
+};
+
+export { useDarkMode };
+
