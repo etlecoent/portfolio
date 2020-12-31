4788b842ed7fd2bafa03ef88d7dac63012081bd8
Etienne LC <etlecoent@gmail.com>
2020-12-31 15:18:35 -0500

    Created template for Home page

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,17 @@
+
+const Home = (props) => {
+
+  return (
+    <section className="min-h-screen pt-16 bg-ysosw dark:bg-ysosb shadow text-black dark:text-white">
+      <div className="min-h-screen mx-auto py-8 px-8 flex justify-center items-center">
+        <div className="max-w-full flex items-center justify-center">
+          
+          <p className="max-w-full text-center text-5xl font-bold">Work in Progress</p>
+          
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default Home;
