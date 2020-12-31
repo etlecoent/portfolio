2d3ea0f7900a777b020447f4028989317476dc77
Etienne LC <etlecoent@gmail.com>
2020-12-31 11:16:48 -0500

    Fixed navBar

--- frontend/src/containers/NavBar.js
+++ frontend/src/containers/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = (props) => {
   const {darkMode, setDarkMode } = props;
   
   return (
-    <nav className="bg-gray-800">
+    <nav className="bg-gray-800 fixed w-full z-40">
       <div className="max-w-7xl mx-auto px-8">
         <div className="flex items-center justify-between h-16">
           <div className="w-full justify-between flex items-center">
