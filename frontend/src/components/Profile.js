f5dbd2097e85e4cf301e593a9bde4b47f018191b
Etienne LC <etlecoent@gmail.com>
2021-01-03 15:45:13 -0500

    Added hiding images when not fully loaded

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 import picture from "../images/picture.jpg"
 
 export default function Profile () {
 
+  const [loaded, setLoaded] = useState(false);
+
+
   return (
     
     <div className="overflow-hidden shadow-lg rounded-lg relative w-64 mb-5">
-        <img alt="profile" src={picture} className="rounded-lg"/>
+        <img 
+          alt="profile" 
+          src={picture} 
+          className="rounded-lg" 
+          style={loaded ? {} : {display: 'none'}}
+          onLoad={() => setLoaded(true)}
+          />
         <div className="absolute bg-gradient-to-b bg-opacity-60 from-transparent to-black w-full p-4 bottom-0">
             <p className="text-white text-2xl nb-4">
                 Étienne LC
--- frontend/src/components/Tool.js
+++ frontend/src/components/Tool.js
@@ -1,10 +1,22 @@
-import { PreviousMap } from "postcss";
-
+import { useState } from "react";
 
 export default function Tool(props) {
+  
   const { src, alt} = props;
+  
+  const [loaded, setLoaded] = useState(false);
 
   return (
-    <img src={src} alt={alt} className="h-14 px-5 pb-5"/>
+    <>
+    
+      <img
+        src={src}
+        alt={alt}
+        onLoad={() => setLoaded(true)}
+        className="h-14 px-5 pb-5"
+        style={loaded ? {} : {display: 'none'}}
+      />
+    
+    </>
   )
 }
