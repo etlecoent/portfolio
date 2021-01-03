72431229b0c4e445fd8240371f8d0101610d638d
Etienne LC <etlecoent@gmail.com>
2021-01-03 16:42:38 -0500

    Added growDown animation on menu in nav bar

--- frontend/src/containers/NavBar.js
+++ frontend/src/containers/NavBar.js
@@ -46,7 +46,7 @@ const NavBar = (props) => {
             </div>
           </div>
         <div className="-mr-2 flex md:hidden">
-          <button className="text-gray-300 text-white inline-flex items-center justify-center p-2 rounded-md focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
+          <button className="text-gray-300 hover:text-white inline-flex items-center justify-center p-2 rounded-md focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
             <svg width="20" height="20" fill="currentColor" className="h-8 w-8" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z">
                 </path>
@@ -56,7 +56,7 @@ const NavBar = (props) => {
       </div>
     </div>
       {isOpen && (
-        <div className="md:hidden">
+        <div className="md:hidden origin-top transition-all growDown ">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <div className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
               <Toggle state={darkMode} handleClick={() => setDarkMode(!darkMode)}/>
