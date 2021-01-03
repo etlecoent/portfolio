416c3234d9c32c170703d68845a765faf05062e9
Etienne LC <etlecoent@gmail.com>
2021-01-03 12:56:28 -0500

    Edited layout and added a new section

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -4,17 +4,20 @@ const Home = (props) => {
 
   return (
     <section className="min-h-screen pt-16 bg-ysosw dark:bg-ysosb shadow text-black dark:text-white">
-      <div className="min-h-screen mx-auto py-8 px-8 flex pt-16 flex-wrap justify-around items-start">
-        <Profile />
-        <div className=" flex flex-col items-center justify-center h-auto w-auto">
-          <h1 className="max-w-full text-center text-4xl font-bold pb-10">
-            M.Eng & Full-Stack Dev
-          </h1>
-          <div className="max-w-md">
-            <p className = "text-center">
-              Curious about technology since my childhood, I chose to become a general engineer in order to better understand the workings of our modern world. Along the way, I discovered an interest in project management and a real passion for the Web which lead me to finish my schooling with a Web Dev bootcamp in order to become a Full Stack Developer.
-            </p>
+      <div className="flex flex-col min-h-screen mx-auto py-8 px-8 pt-16 ">
+        <div className = "flex flex-row flex-wrap justify-around">
+          <Profile />
+          <div className=" flex flex-col items-center justify-center h-auto w-auto">
+            <h1 className="max-w-full text-center text-4xl font-bold pb-10">
+              M.Eng & Full-Stack Dev
+            </h1>
+              <p className = "text-center max-w-md">
+                Curious about technology since my childhood, I chose to become a general engineer in order to better understand the workings of our modern world. Along the way, I discovered an interest in project management and a real passion for the Web which lead me to finish my schooling with a Web Dev bootcamp in order to become a Full Stack Developer.
+              </p>
           </div>
+        </div>
+        <div className = "flex align-center justify-center width-full h-auto pt-16">
+          yooooooooooooo
         </div>
       </div>
     </section>
