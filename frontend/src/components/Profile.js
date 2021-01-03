b7bfdebb8decc808ac52d9d004f2af7625de20d5
Etienne LC <etlecoent@gmail.com>
2021-01-03 12:34:54 -0500

    Created Reachout page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -4,14 +4,14 @@ export default function Profile () {
 
   return (
     
-    <div class="overflow-hidden shadow-lg rounded-lg relative w-64 ">
-        <img alt="profile" src={picture} class="rounded-lg"/>
-        <div class="absolute bg-gradient-to-b bg-opacity-60 from-transparent to-black w-full p-4 bottom-0">
-            <p class="text-white text-2xl nb-4">
+    <div className="overflow-hidden shadow-lg rounded-lg relative w-64 ">
+        <img alt="profile" src={picture} className="rounded-lg"/>
+        <div className="absolute bg-gradient-to-b bg-opacity-60 from-transparent to-black w-full p-4 bottom-0">
+            <p className="text-white text-2xl nb-4">
                 Étienne LC
             </p>
-            <div class="flex justify-between">
-              <p class="text-sm text-gray-300 flex items-center">
+            <div className="flex justify-between">
+              <p className="text-sm text-gray-300 flex items-center">
                 Montréal
               </p>
             </div>
