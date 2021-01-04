9f8d2c8a70f3930c50adff537ab77680b7c8a2c5
Etienne LC <etlecoent@gmail.com>
2021-01-04 09:04:53 -0500

    Removed squared outline on focus

--- frontend/src/components/Toggle.js
+++ frontend/src/components/Toggle.js
@@ -7,7 +7,7 @@ const Toggle = (props) => {
     
     <div>
       <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-        <input onChange={handleClick} type="checkbox" checked={state} name="darkMode" id="prefix-21" className="checked:right-0 checked:bg-blue-600 absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
+        <input onChange={handleClick} type="checkbox" checked={state} name="darkMode" id="prefix-21" className="checked:right-0 checked:bg-blue-600 absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer outline-none"/>
         <label htmlFor="prefix-21" className="block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer">
         </label>
       </div>
