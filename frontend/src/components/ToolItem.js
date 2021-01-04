31ada4e4e1187d8e59a9db847f683268ebdbddc9
Etienne LC <etlecoent@gmail.com>
2021-01-04 11:27:45 -0500

    Added scaling effect on hover

--- frontend/src/components/ToolItem.js
+++ frontend/src/components/ToolItem.js
@@ -13,7 +13,7 @@ export default function Tool(props) {
         src={src}
         alt={alt}
         onLoad={() => setLoaded(true)}
-        className="h-14 px-5 pb-5 opacity-80 hover:opacity-100"
+        className="h-14 px-5 pb-5 opacity-80 hover:opacity-100 transition-all transform hover:scale-110"
         style={loaded ? {} : {display: 'none'}}
       />
     </a>
