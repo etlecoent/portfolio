201ff7f2b240683c02d703cf2907ceefdc65d17c
Etienne LC <etlecoent@gmail.com>
2021-01-05 12:25:50 -0500

    Moved inside logic in Error component

--- frontend/src/containers/Page404.js
+++ frontend/src/containers/Page404.js
@@ -1,15 +1,10 @@
+import Error from "../components/Error";
 
 const Page404 = (props) => {
 
   return (
     <section className="min-h-screen pt-16 bg-ysosw dark:bg-ysosb text-black dark:text-white">
-      <div className="min-h-screen mx-auto py-8 px-8 flex justify-center items-center">
-        <div className="max-w-full flex items-center justify-center">
-          
-          <p className="max-w-full text-center text-5xl font-bold">404 Not Found</p>
-          
-        </div>
-      </div>
+      <Error />
     </section>
   )
 };
