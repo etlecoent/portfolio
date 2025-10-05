f2727ea3429f66cbf514889550c5838e8a4474b0
Etienne Le Coent <etlecoent@gmail.com>
2025-10-05 14:13:08 -0400

    feat: use dark theme

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,10 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   typography: {
     fontFamily: 'var(--font-roboto)'
+  },
+
+  palette: {
+    mode: 'dark'
   }
 });
 
