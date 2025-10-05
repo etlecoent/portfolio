a18c17723faac19d7f3c4b5bef15156f08fe3172
Etienne Le Coent <etlecoent@gmail.com>
2025-10-05 14:33:59 -0400

    chore: remove duplicate container

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -54,18 +54,16 @@ export default async function Projects() {
   projects = projects.filter((p) => p.owner.login === GITHUB_PROFILE);
 
   return (
-    <Container>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-      >
-        {projects.map((project) => (
-          <Grid key={project.id} size={{ xs: 4, sm: 4, md: 4 }}>
-            <Project project={project} />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <Grid
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 4, sm: 8, md: 12 }}
+    >
+      {projects.map((project) => (
+        <Grid key={project.id} size={{ xs: 4, sm: 4, md: 4 }}>
+          <Project project={project} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
