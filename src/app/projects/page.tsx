686a84f25f95c0ab505d2ac57a1e0d6d96a14159
Etienne Le Coent <etlecoent@gmail.com>
2025-12-13 17:49:03 -0500

    feat: add caption

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,6 +3,7 @@ import type { Project as ProjectType } from '@/types/Project';
 import Grid from '@mui/material/Grid';
 
 import Project from './Project';
+import { Typography } from '@mui/material';
 
 const GITHUB_PROFILE = process.env.GITHUB_PROFILE!;
 
@@ -15,16 +16,23 @@ export default async function Projects() {
   projects = projects.filter((p) => p.owner.login === GITHUB_PROFILE);
 
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-    >
-      {projects.map((project) => (
-        <Grid key={project.id} size={{ xs: 4, sm: 4, md: 4 }}>
-          <Project project={project} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid textAlign='center' mb={4}>
+        <Typography gutterBottom variant='h5'>
+          Here&apos;s a list of my starred projects on GitHub!
+        </Typography>
+      </Grid>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {projects.map((project) => (
+          <Grid key={project.id} size={{ xs: 4, sm: 4, md: 4 }}>
+            <Project project={project} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
