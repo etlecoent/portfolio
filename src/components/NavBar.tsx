2be754a089a8d2fbfecd40cb56b107c7e5dac41d
Etienne Le Coent <etlecoent@gmail.com>
2025-12-13 10:29:17 -0500

    feat: add logo to nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,10 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import Link from 'next/link';
+import Image from 'next/image';
+import { Typography } from '@mui/material';
 
 const MENU_ITEMS = [
   { label: 'Home', href: '/' },
@@ -14,17 +17,17 @@ export default function NavBar() {
     <AppBar position='sticky' component='nav'>
       <Toolbar>
         <Box>
-          {MENU_ITEMS.map(({ label, href }) => (
-            <Button
-              key={label}
-              color='secondary'
-              href={href}
-              LinkComponent={Link}
-            >
-              {label}
-            </Button>
-          ))}
+          <IconButton disabled>
+            <Image src='/logo.png' alt='logo' width={50} height={50} />
+          </IconButton>
         </Box>
+        {MENU_ITEMS.map(({ label, href }) => (
+          <Box key={label}>
+            <Button color='primary' href={href} LinkComponent={Link}>
+              <Typography variant='body1'>{label}</Typography>
+            </Button>
+          </Box>
+        ))}
       </Toolbar>
     </AppBar>
   );
