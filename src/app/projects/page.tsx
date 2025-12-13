d135a46f05486000bdec471595873c06c5b15df3
Etienne Le Coent <etlecoent@gmail.com>
2025-12-13 17:39:43 -0500

    feat: fetch github projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,56 +1,17 @@
 import type { Project as ProjectType } from '@/types/Project';
 
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 
 import Project from './Project';
 
 const GITHUB_PROFILE = process.env.GITHUB_PROFILE!;
 
-const MOCK_PROJECTS: ProjectType[] = [
-  {
-    id: 1,
-    name: 'Project One',
-    html_url: 'yolo',
-    description: 'This is project one',
-    owner: { login: GITHUB_PROFILE, id: 1 }
-  },
-  {
-    id: 2,
-    name: 'Project Two',
-    html_url: 'yolo',
-    description: 'This is project two',
-    owner: { login: GITHUB_PROFILE, id: 1 }
-  },
-  {
-    id: 3,
-    name: 'Project Three',
-    html_url: 'yolo',
-    description: 'This is project three',
-    owner: { login: GITHUB_PROFILE, id: 1 }
-  },
-  {
-    id: 4,
-    name: 'Project Three',
-    html_url: 'yolo',
-    description: 'This is project three',
-    owner: { login: GITHUB_PROFILE, id: 1 }
-  },
-  {
-    id: 5,
-    name: 'Project Three',
-    html_url: 'yolo',
-    description: 'This is project three',
-    owner: { login: GITHUB_PROFILE, id: 1 }
-  }
-];
-
 export default async function Projects() {
-  // const data = await fetch(
-  //   `https://api.github.com/users/${GITHUB_PROFILE}/starred`
-  // );
-  // let projects: ProjectType[] = await data.json();
-  let projects: ProjectType[] = MOCK_PROJECTS;
+  const data = await fetch(
+    `https://api.github.com/users/${GITHUB_PROFILE}/starred`,
+    { cache: 'force-cache' }
+  );
+  let projects: ProjectType[] = await data.json();
   projects = projects.filter((p) => p.owner.login === GITHUB_PROFILE);
 
   return (
